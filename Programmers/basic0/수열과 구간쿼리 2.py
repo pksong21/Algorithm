9ca7c5f8b51b62e14d9d1350864fa092720a91d2
pksong21 <songp875@gmail.com>
2023-11-10 14:21:45 +0900

    11.10

--- Programmers/basic0/수열과 구간쿼리 2.py
+++ Programmers/basic0/수열과 구간쿼리 2.py
@@ -1,14 +1,20 @@
+# 수열과 구간 쿼리 2
+# 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e, k] 꼴입니다.
+# 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 k보다 크면서 가장 작은 arr[i]를 찾습니다.
+# 각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요.
+# 단, 특정 쿼리의 답이 존재하지 않으면 -1을 저장합니다.
+#
 
 def solution(arr, queries):
     answer = []
     for i in queries:
-        arr2=[]
+        arr2=[-1]
         for j in range(i[0], i[1]+1):
             if arr[j]>i[2]:
-                arr2.append(arr[j])
-        if len(arr2)==0:
-            answer.append(-1)
-            continue
-        arr2.sort()
+                if arr2[0]==-1:
+                    if arr2[0]<arr[j]:
+                        arr2[0]=arr[j]
+                elif arr2[0]>arr[j]:
+                    arr2[0]=arr[j]
         answer.append(arr2[0])
     return answer
