9a210c15f30ef8d504cc66c04a7c59abdcd114d4
pksong21 <133176946+pksong21@users.noreply.github.com>
2023-11-13 14:36:10 +0900

    Update 수열과 구간쿼리4.py
    11.13

--- Programmers/basic0/수열과 구간쿼리4.py
+++ Programmers/basic0/수열과 구간쿼리4.py
@@ -3,3 +3,9 @@
 # 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 i가 k의 배수이면 arr[i]에 1을 더합니다.
 # 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
 
+def solution(arr, queries):
+    for s, e, k in queries:
+        for i in range(s, e+1):
+            if i % k == 0:
+                arr[i] += 1
+    return arr
